aee186ef7c8a6173e96668564a54a027b5588685
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-05-03 02:39:20 +0200

    resolve rondom answers

--- components/RealQuiz.jsx
+++ components/RealQuiz.jsx
@@ -1,5 +1,6 @@
-'use client'
-import { useEffect, useState } from 'react';
+'use client';
+
+import { useEffect, useState } from'react';
 import { Next, Previous } from '@/components/globals/Icons';
 import CountdownTimer from './CountdownTimer';
 import { FormulateQuote } from '@/utils/services';
@@ -16,34 +17,34 @@ export default function RealQuiz({ QuizQuestions }) {
     if (animateScore) {
       const timeoutId = setTimeout(() => {
         setAnimateScore(false);
-      }, 500); // dlration of the animation
+      }, 500); 
       return () => clearTimeout(timeoutId);
     }
   }, [animateScore]);
 
-  const handleAnswer = (answer, correctAnswer, index) => {
+  const handleAnswer = (answer, index) => {
     setAnimateScore(true);
     const newSelectedAnswers = [...selectedAnswers];
     newSelectedAnswers[index] = answer;
     setSelectedAnswers(newSelectedAnswers);
-  
+
     if (answer === correctAnswer) {
-      setScore((prevScore) => prevScore + 1); // Use functional update form
+      setScore((prevScore) => prevScore + 1); 
     }
-  
+
     const nextQuestionIndex = currentQuestionIndex + 1;
     if (nextQuestionIndex < QuizQuestions.length) {
       setTimeout(() => {
         setCurrentQuestionIndex(nextQuestionIndex);
       }, 300);
-  
+
     } else {
       setTimeout(() => {
-        alert(`Quiz finished! Your score is ${score + (answer === correctAnswer ? 1 : 0)} out of ${QuizQuestions.length}`);
+        alert(`Quiz finished! Your score is ${score + (answer === correctAnswer? 1 : 0)} out of ${QuizQuestions.length}`);
       }, 300);
     }
   };
-  
+
   const handlePrevious = () => {
     setBackAnimateScore(true)
 
@@ -64,21 +65,34 @@ export default function RealQuiz({ QuizQuestions }) {
     }
   };
 
-  const { question, answers } = QuizQuestions[currentQuestionIndex];
-  const shuffledAnswers = useShuffle(answers);
-  const ass = FormulateQuote(question);
+  const [shuffledAnswers, setShuffledAnswers] = useState([]);
+  const [correctAnswer, setCorrectAnswer] = useState(null);
+
+  useEffect(() => {
+    const { question, answers } = QuizQuestions[currentQuestionIndex];
+    setCorrectAnswer(answers[0]); 
+    const incorrectAnswers = answers.slice(1);
+    const allAnswers = [...incorrectAnswers, answers[0]]; 
+    for (let i = allAnswers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [allAnswers[i], allAnswers[j]] = [allAnswers[j], allAnswers[i]];
+    }
+    setShuffledAnswers(allAnswers);
+  }, [currentQuestionIndex, QuizQuestions]);
+
+  const ass = FormulateQuote(QuizQuestions[currentQuestionIndex].question);
 
   return (
     <div className="py-4">
-      <div className={` ${animateScore ? 'score-animate' : ''} ${BackanimateScore&& 'back-score-animate'}  border-l border-[#DEA78C] p-4 shadow-md`}>
+      <div className={` ${animateScore?'score-animate' : ''} ${BackanimateScore&& 'back-score-animate'}  border-l border-[#DEA78C] p-4 shadow-md`}>
         <h2 className=" text-base xs:text-lg sm:text-xl lg:text-2xl font-bold " style={{ whiteSpace: 'pre-line' }}>{ass}</h2>
       </div>
       <ul className='absolute left-1/2 -translate-x-1/2 bottom-4 flex flex-col flex-wrap w-full px-4 gap-4 justify-center text-sm'>
        
-        <div className={`flex flex-row justify-center flex-wrap w-full gap-2 ${animateScore ? 'score-animate' : ''} ${BackanimateScore&& 'back-score-animate'} `}>
+        <div className={`flex flex-row justify-center flex-wrap w-full gap-2 ${animateScore?'score-animate' : ''} ${BackanimateScore&& 'back-score-animate'} `}>
           {shuffledAnswers.map((answer, index) => (
             <li key={index} className="">
-              <button className={`transition duration-100 text-black font-bold py-2 px-4 rounded-[1.5rem] ${selectedAnswers[currentQuestionIndex] === answer ? 'bg-[#DEA78C]' : 'bg-[#D0D0D0] hover:bg-[#B7B7B7]'}`} onClick={() => handleAnswer(answer, answers[0], currentQuestionIndex)}>
+              <button className={`transition duration-100 text-black font-bold py-2 px-4 rounded-[1.5rem] ${selectedAnswers[currentQuestionIndex] === answer? 'bg-[#DEA78C]' : 'bg-[#D0D0D0] hover:bg-[#B7B7B7]'}`} onClick={() => handleAnswer(answer, currentQuestionIndex)}>
                 {answer}
               </button>
             </li>
@@ -87,51 +101,24 @@ export default function RealQuiz({ QuizQuestions }) {
 
         <div className="flex justify-center gap-4 items-center text-center w-full mt-4 space-x-2">
           <div className='flex flex-row gap-2'>
-          <button className={`transition duration-100 text-white font-bold p-2 rounded-full ${currentQuestionIndex === 0 ? 'bg-transparent' : 'bg-gray-900 hover:bg-gray-800'}`} onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
+          <button className={`transition duration-100 text-white font-bold p-2 rounded-full ${currentQuestionIndex === 0? 'bg-transparent' : 'bg-gray-900 hover:bg-gray-800'}`} onClick={handlePrevious} disabled={currentQuestionIndex === 0}>
             <Previous />
           </button>
-          <button className={`transition duration-100 text-white font-bold p-2 rounded-full ${currentQuestionIndex === QuizQuestions.length - 1 ? 'bg-transparent' : 'bg-gray-900 hover:bg-gray-800'}`} onClick={handleNext} disabled={currentQuestionIndex === QuizQuestions.length - 1}>
+          <button className={`transition duration-100 text-white font-bold p-2 rounded-full ${currentQuestionIndex === QuizQuestions.length - 1? 'bg-transparent' : 'bg-gray-900 hover:bg-gray-800'}`} onClick={handleNext} disabled={currentQuestionIndex === QuizQuestions.length - 1}>
             <Next />
           </button>
           </div>
-          <button className={` transition duration-200 rounded-[2rem]  px-2 md:px-4 py-1 text-sm md:text-base md:py-2  font-bold ${currentQuestionIndex != QuizQuestions.length - 1 ? 'bg-gray-800' : 'hover:bg-[#AD7974] bg-[#B2847A]'} `}
-           disabled={currentQuestionIndex != QuizQuestions.length - 1}>Submit</button>
+          <button className={` transition duration-200 rounded-[2rem]  px-2 md:px-4 py-1 text-sm md:text-base md:py-2  font-bold ${currentQuestionIndex!= QuizQuestions.length - 1? 'bg-gray-800' : 'hover:bg-[#AD7974] bg-[#B2847A]'} `}
+           disabled={currentQuestionIndex!= QuizQuestions.length - 1}>Submit</button>
         </div>
 
       </ul>
       <h2 className={`h-14 absolute top-16 right-2 md:top-20 md:right-10 px-2 text-base xs:text-lg sm:text-xl lg:text-2xl font-bold `}>
-        <span className={`${animateScore ? 'score-animate' : ''}`}>
+        <span className={`${animateScore?'score-animate' : ''}`}>
         {currentQuestionIndex + 1}
         </span>
         /{QuizQuestions.length}
       </h2>
     </div>
   );
-}
-
-function useShuffle(array) {
-  const [shuffledArray, setShuffledArray] = useState([]);
-
-  useEffect(() => {
-    // had to do this because Error: Text content does not match server-rendered HTML. dafuq
-    const shuffled = shuffle(array);
-    setShuffledArray(shuffled);
-  }, [array]);
-
-  return shuffledArray;
-}
-
-function shuffle(array) {
-  let currentIndex = array.length, randomIndex;
-
-  while (currentIndex != 0) {
-
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
 }
