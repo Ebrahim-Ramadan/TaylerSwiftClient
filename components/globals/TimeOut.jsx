7eb9a28c399d761e2247505929279b72d8f62eba
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2024-05-05 22:09:29 +0200

    ass

--- components/globals/TimeOut.jsx
+++ components/globals/TimeOut.jsx
@@ -9,10 +9,11 @@ export function TimeOutComponent(
       {Quizfinished ?
         
         <div className="space-y-4 text-center">
-        <h1 className="text-3xl font-bold text-[#B2847A]">Just Finished, Congrats!</h1>
-        <p className="text-[#7b7b7b] dark:text-[#a0a0a0]">
-         You scored {score} / {total}
-        </p>
+        <p className="text-[#B2847A] font-bold text-xl">
+            scored <span className=" text-2xl">{score}/ {total}</span> 
+          </p>
+        <h1 className="text-xl  text-[#B2847A]"> {score>6?'Congrats✨':'not real swifitie tho'} </h1>
+          
       </div>
       :
       <div className="space-y-4 text-center">
@@ -32,7 +33,7 @@ export function TimeOutComponent(
         </Link>
         <button
           onClick={()=> window.location.reload()}
-          className="inline-flex  items-center justify-center rounded-[2rem] border border-[#c13483] bg-white px-2 md:px-4 py-1 text-sm md:text-base md:py-2  font-medium shadow-sm transition-colors hover:bg-[#f0f0f0] hover:text-[#c13483] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#c13483] disabled:pointer-events-none disabled:opacity-50 dark:border-[#7b7b7b] dark:bg-[#1e1e1e] dark:hover:bg-[#2e2e2e] dark:hover:text-[#f0f0f0]"
+          className="inline-flex  items-center justify-center rounded-[2rem] border border-[#c13483] bg-black px-2 md:px-4 py-1 text-sm md:text-base md:py-2  font-medium shadow-sm transition-colors hover:bg-[#f0f0f0] hover:text-[#c13483] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#c13483] disabled:pointer-events-none disabled:opacity-50 dark:border-[#7b7b7b] dark:bg-[#1e1e1e] hover:bg-[#2e2e2e] dark:hover:text-[#f0f0f0]"
         >
           Retake Quiz
         </button>
