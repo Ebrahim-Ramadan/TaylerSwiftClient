dafb9e827d398e5df0c8b3d73d243e5e31c44d8a
Mohammed Mostafa <medo.mostafa22255@gmail.com>
2024-05-06 20:45:10 +0200

    resloved fucking twitter photo

--- app/og/route.js
+++ app/og/route.js
@@ -1,4 +1,6 @@
 import { ImageResponse } from 'next/og';
+import taylor from '@/public/taylor.jpg'
+
 
 export async function GET(request) {
   return new ImageResponse(
@@ -6,18 +8,16 @@ export async function GET(request) {
       <img
         width="100%"
         height="100%"
-        src='https://i.pinimg.com/originals/19/32/e2/1932e2224970e57d6bb5d35faebf570d.jpg'
+        src='taylor.jpg'
        
       />
     ),
     {
-      width: 1200,
-      height: 1000,
-      headers: {
+     headers: {
         'Content-Type': 'image/jpeg',
         'Cache-Control': 'public, max-age=31536000', // cache for 1 year
         'Twitter-Card':'summary_large_image',
-        'Twitter-Image': 'https://i.pinimg.com/originals/19/32/e2/1932e2224970e57d6bb5d35faebf570d.jpg',
+        'Twitter-Image': 'taylor.jpg',
       },
     },
   );
